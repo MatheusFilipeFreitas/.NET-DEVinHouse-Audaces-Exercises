e8d4d76a2e525ede87f6470f2ae82497fa6475fa
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 15:44:49 -0300

    M2S2E04

---
+++ M2S2E04/M2S2E04/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S2E04
+{
+    internal class Car
+    {
+        private string name;
+        private string model;
+        private float kilometers;
+
+        public Car(string name, string model, float kilometers)
+        {
+            this.name = name;
+            this.model = model;
+            this.kilometers = kilometers;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Model { get => model; set => model = value; }
+        public float Kilometers { get => kilometers; set => kilometers = value; }
+
+        public string Info()
+        {
+            return name + " - " +  model + " - " + kilometers;
+        }
+    }
+}
---
+++ M2S2E04/M2S2E04/Program.cs
@@ -0,0 +1,20 @@
+﻿using M2S2E04;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o nome: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Digite o modelo: ");
+        string model = Console.ReadLine();
+
+        Console.WriteLine("Km rodados: ");
+        float km = float.Parse(s: Console.ReadLine());
+
+        Car car = new Car(name, model, km);
+
+        Console.WriteLine(car.Info());
+    }
+}
