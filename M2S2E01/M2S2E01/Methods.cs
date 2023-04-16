a14fd365c7416436c32ae8d385c2ce29775afbe1
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 15:18:13 -0300

    Adicionar arquivos de projeto.

---
+++ M2S2E01/M2S2E01/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S2E01
+{
+    internal class Methods
+    {
+        public int Sum (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ M2S2E01/M2S2E01/Program.cs
@@ -0,0 +1,17 @@
+﻿using M2S2E01;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Methods method = new Methods();
+
+        Console.WriteLine("Numero 1: ");
+        int num1 = int.Parse(s: Console.ReadLine());
+
+        Console.WriteLine("Numero 2: ");
+        int num2 = int.Parse(s: Console.ReadLine());
+
+        Console.WriteLine("Result: " + method.Sum(num1, num2));
+    }
+}
