e412911b346ef3621646e372a93d2b5403b414a1
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 15:35:00 -0300

    M2S2E03

---
+++ M2S2E03/M2S2E03/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o nome: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Digite a idade: ");
+        int years = int.Parse(s: Console.ReadLine());
+
+        Console.WriteLine(name);
+        Console.WriteLine(years);
+        if( years < 18)
+        {
+            Console.WriteLine("Menor de idade");
+        }
+        else if( years < 60)
+        {
+            Console.WriteLine("Maior de idade");
+        }else
+        {
+            Console.WriteLine("Idoso");
+        }
+    }
+}
