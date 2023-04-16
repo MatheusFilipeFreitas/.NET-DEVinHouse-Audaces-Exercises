21a0d655e9da79ca0b70fbc3ee4f61ec4bcaa1d1
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 16:11:55 -0300

    M2S2E05

---
+++ M2S2E05/M2S2E05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ArrayList nums = new ArrayList ();
+
+        for(int i = 0; i < 4; i++)
+        {
+            Console.WriteLine("Numero " + (i + 1) + ":" );
+            int num = int.Parse(s: Console.ReadLine());
+            nums.Add (num);
+        }
+
+        nums.Sort();
+        Console.WriteLine();
+        foreach(int element in nums)
+        {
+            Console.Write(element + " ");
+        }
+    }
+}
