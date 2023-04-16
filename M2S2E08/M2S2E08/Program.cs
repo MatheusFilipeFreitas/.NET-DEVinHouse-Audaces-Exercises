0db6475aa606c33a425ed0cb78172dc6a40ce0de
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 20:48:11 -0300

    M2S2E08

---
+++ M2S2E08/M2S2E08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[5];
+        
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("Numero " + (i+1) + ": ");
+            int num = int.Parse(Console.ReadLine());
+            numbers[i] = num;
+        }
+
+        Array.Sort(numbers);
+        Array.Reverse(numbers);
+
+        foreach(int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
