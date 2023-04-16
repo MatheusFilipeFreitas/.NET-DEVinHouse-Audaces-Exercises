68c1a9f7c2edbfa2e1c5b874abd9d7dd73076ee7
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 20:21:10 -0300

    M2S2E06

---
+++ M2S2E06/M2S2E06/Program.cs
@@ -0,0 +1,17 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string phrase = "A linguagem de programação C# é muito poderosa";
+        string[] words = phrase.Split(" ");
+
+        foreach (string word in words)
+        {
+            if(word.Equals("poderosa"))
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
