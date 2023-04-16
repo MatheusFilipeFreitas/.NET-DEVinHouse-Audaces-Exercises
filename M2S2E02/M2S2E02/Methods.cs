87dae0a7e73c96649cd22ea5a8b177600b3c9dca
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 15:24:45 -0300

    M2S2E02

---
+++ M2S2E02/M2S2E02/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S2E02
+{
+    internal class Methods
+    {
+        public Boolean isOdd(int num)
+        {
+            return (num % 2 == 0);
+        }
+    }
+}
---
+++ M2S2E02/M2S2E02/Program.cs
@@ -0,0 +1,19 @@
+﻿using M2S2E02;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Methods method = new Methods();
+        Console.WriteLine("Numero: ");
+        int num = int.Parse(s: Console.ReadLine());
+        if(method.isOdd(num))
+        {
+            Console.WriteLine("Odd");
+        }
+        else
+        {
+            Console.WriteLine("Even");
+        }
+    }
+}
