751bf74724cdbd8e55492198e5780da3a7b012cb
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-16 22:41:08 -0300

    M2S2E09

---
+++ M2S2E09/M2S2E09/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a quantidade: ");
+        int length = int.Parse(Console.ReadLine());
+
+        float[] numbers = new float[length];
+        float sum = 0;
+
+        for(int i = 0; i < length; i++)
+        {
+            Console.WriteLine("Digite o numero: ");
+            float num = float.Parse(Console.ReadLine());
+
+            numbers[i] = num;
+        }
+
+        foreach(float num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine("Result: " + (sum /= length));
+    }
+}
