749ad708279a1d4ebafdcc0cdb461aae3af0e010
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-23 08:50:28 -0300

    M2S3E02

---
+++ M2S3E02/M2S3E02/Methods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S3E01
+{
+    internal class Methods
+    {
+        public double Sum (double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Sum(double num1, double num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
---
+++ M2S3E02/M2S3E02/Program.cs
@@ -0,0 +1,20 @@
+﻿using M2S3E01;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Methods method = new Methods();
+
+        Console.WriteLine("Numero 1: ");
+        int num1 = int.Parse(s: Console.ReadLine());
+
+        Console.WriteLine("Numero 2: ");
+        int num2 = int.Parse(s: Console.ReadLine());
+
+        Console.WriteLine("Numero 3: ");
+        int num3 = int.Parse(s: Console.ReadLine());
+
+        Console.WriteLine("Result: " + method.Sum(num1, num2, num3));
+    }
+}
