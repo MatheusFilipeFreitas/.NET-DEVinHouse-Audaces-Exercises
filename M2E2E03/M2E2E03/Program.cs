32ab13a53b24acb4f1ca7d276c55a01a4dfc6971
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-23 09:05:12 -0300

    M2S3E03

---
+++ M2E2E03/M2E2E03/Program.cs
@@ -0,0 +1,9 @@
+﻿using M2E2E03;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ M2E2E03/M2E2E03/Proprietario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2E2E03
+{
+    internal class Proprietario
+    {
+        public DateTime id { get; set; }
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public string telefone { get; set; }
+
+        public Proprietario() { }
+
+        public Proprietario(string nome, string cpf, string telefone)
+        {
+            this.id = DateTime.Now;
+            this.nome = nome;
+            this.cpf = cpf;
+            this.telefone = telefone;
+        }
+    }
+}
