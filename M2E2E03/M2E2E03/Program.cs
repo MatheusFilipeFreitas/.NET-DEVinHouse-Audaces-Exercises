09eba5dd46b4986d7a8cf5c575f8b77778c070de
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-04-23 09:15:47 -0300

    M2S3E04

--- M2E2E03/M2E2E03/Program.cs
+++ M2E2E03/M2E2E03/Program.cs
@@ -4,6 +4,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Proprietario proprietario1 = new Proprietario("Teste", "123456789101","55999999999");
+        Proprietario proprietario2 = new Proprietario("Teste2", "123456789101", "55999999999");
+        Proprietario proprietario3 = new Proprietario("Teste3", "123456789101", "55999999999");
     }
 }
