520de004eac0d10ced9c998ce61ba7431ee8e406
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 17:51:01 -0300

    M2S4E10

---
+++ M2S4E10/M2S4E10/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E10
+{
+    internal class ContaBancaria
+    {
+        private double _saldo;
+        private List<Transacao> transacaos = new List<Transacao>();
+        public void Depositar(double value)
+        {
+            _saldo += value;
+            Transacao transacao = new Transacao();
+            transacao.Nome = "Deposito";
+            transacao.Date = new DateOnly();
+            transacao.Valor = value;
+            transacaos.Add(transacao);
+        }
+
+        public void Sacar(double value)
+        {
+            if(value <= _saldo)
+            {
+                _saldo -= value;
+                Transacao transacao = new Transacao();
+                transacao.Nome = "Saque";
+                transacao.Date = new DateOnly();
+                transacao.Valor = value;
+                transacaos.Add(transacao);
+            }
+        }
+
+        public double getSaldo()
+        {
+            return _saldo;
+        }
+
+        public void HistoricoTransacoes()
+        {
+            foreach(Transacao transacao in transacaos)
+            {
+                Console.WriteLine(value: $"[{transacao.Date}] {transacao.Nome} - valor: {transacao.Valor}");
+            }
+        }
+
+    }
+}
---
+++ M2S4E10/M2S4E10/Transacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E10
+{
+    internal class Transacao
+    {
+        private string _nome;
+        private DateOnly _date;
+        private double _valor;
+
+        public string Nome { get { return _nome; } set { _nome = value; } }
+        public DateOnly Date { get { return _date; } set { _date = value; } }
+    
+        public double Valor { get { return _valor; } set { _valor = value; } }
+    }
+}
