1ab8bdf21b53b6c320ecda29478d67707c5402d0
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 16:32:14 -0300

    M2S4E06

---
+++ M2S4E06/M2S4E06/Program.cs
@@ -0,0 +1,17 @@
+﻿using M2S4E06;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Retangulo retangulo = new Retangulo();
+        retangulo.Altura = 15;
+        retangulo.Largura = -10;
+
+        Console.WriteLine(retangulo.Altura);
+        Console.WriteLine(retangulo.Largura);
+
+        retangulo.Largura = 20;
+        Console.WriteLine(retangulo.CalculaArea().ToString());
+    }
+}
---
+++ M2S4E06/M2S4E06/Retangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E06
+{
+    class Retangulo
+    {
+        private double _altura;
+        private double _largura;
+
+        public double Altura {
+            get
+            {
+                return _altura;
+            }
+            set
+            {
+                if(value > 0) _altura = value;
+                else _altura = 0;
+            }
+        }
+
+        public double Largura
+        {
+            get
+            {
+                return _largura;
+            }
+            set
+            {
+                if (value > 0) _largura = value;
+                else _largura = 0;
+            }
+        }
+
+        public double CalculaArea()
+        {
+            return _largura * _altura;
+        }
+    }
+}
