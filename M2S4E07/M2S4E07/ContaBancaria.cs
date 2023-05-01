96674a7153d822b6fa70e1cb9540054266c5c09a
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 17:24:01 -0300

    M2S4E07

---
+++ M2S4E07/M2S4E07/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E07
+{
+    class ContaBancaria
+    {
+        public const double SaldoMax = 1000.00;
+        private int _numeroDaConta;
+        private double _saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                _saldo = value;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor < 0 || (valor + Saldo) > SaldoMax)
+            {
+                Console.WriteLine("Valor nao permitido");
+            }
+            else 
+            { 
+                Saldo += valor;
+            }
+        }
+        public void Sacar(double valor)
+        {
+            if (valor < 0 && valor > Saldo)
+            {
+                Console.WriteLine("Valor nao permitido!");
+            }
+            else 
+            { 
+                Saldo -= valor; 
+            }
+        }
+    }
+}
---
+++ M2S4E07/M2S4E07/Program.cs
@@ -0,0 +1,14 @@
+﻿using M2S4E07;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria();
+        conta.Saldo = 100;
+        conta.Depositar(1500);
+        Console.WriteLine(value: $"{conta.Saldo}");
+        conta.Sacar(500);
+        Console.WriteLine(value: $"{conta.Saldo}");
+    }
+}
