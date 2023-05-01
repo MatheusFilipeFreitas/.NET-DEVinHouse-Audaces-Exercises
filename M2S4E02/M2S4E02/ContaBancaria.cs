5d13e7d0422245b454a0497818433059669c8ecb
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 14:26:54 -0300

    M2S4E02

---
+++ M2S4E02/M2S4E02/ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E02
+{
+    class ContaBancaria
+    {
+        private string numeroConta;
+        private double saldo;
+
+        public ContaBancaria(string numeroConta, double saldo)
+        {
+            this.numeroConta = numeroConta;
+            this.saldo = saldo;
+        }
+
+        public void Deposita(double valor)
+        {
+            saldo += valor;
+        }
+
+        public void Saca(double valor)
+        {
+            if(valor <= saldo)
+            {
+                saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+    }
+}
---
+++ M2S4E02/M2S4E02/Program.cs
@@ -0,0 +1,20 @@
+﻿using M2S4E02;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria("0002830", 1000.0);
+        Console.WriteLine(value: $"Conta criada... Saldo: {conta.GetSaldo()}");
+        conta.Deposita(500);
+        conta.Saca(2000);
+        Console.WriteLine(value: $"Saldo: {conta.GetSaldo()}");
+        conta.Deposita(100);
+        conta.Saca(2000);
+        Console.WriteLine(value: $"Saldo: {conta.GetSaldo()}");
+        conta.Saca(1600);
+        Console.WriteLine(value: $"Saldo: {conta.GetSaldo()}");
+        conta.Saca(200);
+        Console.WriteLine(value: $"Saldo: {conta.GetSaldo()}");
+    }
+}
