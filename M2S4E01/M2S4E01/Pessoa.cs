ff9241a03406b99d947ce72ee12b9dd0e08d6cd3
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 14:11:23 -0300

    M2S4E01

---
+++ M2S4E01/M2S4E01/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E01
+{
+    class Pessoa
+    {
+        private string nome;
+        protected int idade;
+        public string endereco;
+
+        public Pessoa(string nome, int idade, string endereco)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.endereco = endereco;
+        }
+
+        public string Saudacao()
+        {
+            return $"Ola, meu nome é {nome} e eu tenho {idade} anos";
+        }
+    }
+}
---
+++ M2S4E01/M2S4E01/Program.cs
@@ -0,0 +1,10 @@
+﻿using M2S4E01;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa("Bian",23,"Seol");
+        Console.WriteLine(pessoa.Saudacao());
+    }
+}
