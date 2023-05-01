62614c31aa3a957585b3340e01c4f497cc3b5b3b
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 17:37:50 -0300

    M2S4E09

---
+++ M2S4E09/M2S4E09/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E09
+{
+    static class Calculadora
+    {
+
+        public const double Pi = 3.14159265359;
+
+        public static double Somar(double valor1, double valor2)
+        {
+           return valor1 + valor2;
+        }
+
+        public static double Subtracao(double valor1, double valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public static double Multiplicacao(double valor1, double valor2)
+        {
+            return valor1 * valor2;
+        }
+
+        public static double Divisao(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+
+    }
+}
---
+++ M2S4E09/M2S4E09/Program.cs
@@ -0,0 +1,13 @@
+﻿using M2S4E09;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(value: $"{Calculadora.Somar(5, 15)}");
+        Console.WriteLine(value: $"{Calculadora.Multiplicacao(5, 15)}");
+        Console.WriteLine(value: $"{Calculadora.Divisao(5, 15)}");
+        Console.WriteLine(value: $"{Calculadora.Subtracao(5, 15)}");
+        Console.WriteLine(value: $"{Calculadora.Somar(1, Calculadora.Pi)}");
+    }
+}
