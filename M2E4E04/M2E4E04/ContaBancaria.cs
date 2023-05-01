04746ad6349e9478d0db85ca5d3e9700b41dd583
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 15:03:23 -0300

    M2S4E04

---
+++ M2E4E04/M2E4E04/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2E4E04
+{
+    internal class ContaBancaria
+    {
+        private double _saldo;
+
+        public ContaBancaria(double saldo)
+        {
+            if (saldo < 0) { saldo = 0; }
+            else Saldo = saldo;
+        }
+
+        public double Saldo
+        {
+            get => _saldo;
+            set
+            {
+                if (_saldo < 0) { _saldo = 0; }
+                else _saldo = value;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor >= 0)
+            {
+                Saldo += valor;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor >= 0 && valor <= Saldo)
+            {
+                Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ M2E4E04/M2E4E04/Program.cs
@@ -0,0 +1,14 @@
+﻿using M2E4E04;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContaBancaria contaBancaria = new ContaBancaria(-500.00);
+        Console.WriteLine(contaBancaria.Saldo.ToString("C"));
+        contaBancaria.Depositar(300.00);
+        Console.WriteLine(contaBancaria.Saldo.ToString("C"));
+        Console.WriteLine(contaBancaria.Sacar(200.00));
+        Console.WriteLine(contaBancaria.Saldo.ToString("C"));
+    }
+}
