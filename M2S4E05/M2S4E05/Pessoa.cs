22a8cc311ca6489f292b8bba5a6e7c9c349037c8
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-01 16:24:57 -0300

    M2S4E05

---
+++ M2S4E05/M2S4E05/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S4E05
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        private int _idade;
+
+        public int Idade
+        {
+            get { return _idade;}
+            set
+            {
+                if(value > 0) _idade = value;
+                else _idade = 0;
+            }
+        }
+
+        public void DefinirIdade(int value)
+        {
+            if(value > 0) Idade = value;
+        }
+    }
+}
---
+++ M2S4E05/M2S4E05/Program.cs
@@ -0,0 +1,14 @@
+﻿using M2S4E05;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa pessoa = new Pessoa();
+        pessoa.Nome = "Suki";
+        pessoa.Idade = -15;
+        Console.WriteLine(pessoa.Idade);
+        pessoa.DefinirIdade(22);
+        Console.WriteLine(pessoa.Idade);
+    }
+}
