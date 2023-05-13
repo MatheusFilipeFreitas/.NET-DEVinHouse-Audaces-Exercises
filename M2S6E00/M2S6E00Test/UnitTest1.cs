75142ed2848139657e032b08c353e7632aa4e462
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-13 19:00:49 -0300

    M2S6E02

---
+++ M2S6E00/M2S6E00Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace M2S6E00Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
