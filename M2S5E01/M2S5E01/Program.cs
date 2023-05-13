a4a77acec4ca08fe8222e376e9693a7eefbd9e29
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-13 18:48:01 -0300

    M2S5E010

--- M2S5E01/M2S5E01/Program.cs
+++ M2S5E01/M2S5E01/Program.cs
@@ -1,7 +1,56 @@
-﻿internal class Program
+﻿using M2S5E01.Interface;
+using M2S5E01.service;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IClienteService clienteService = new ClienteService();
+
+        string opcao;
+        do
+        {
+            Console.WriteLine("\n\nBem Vindo ao banco Fullstack, Escolha uma opção para continuar: ");
+            Console.WriteLine("1 - Abrir Conta");
+            Console.WriteLine("2 - Consultar Conta");
+            Console.WriteLine("3 - Listar Todas as contas");
+            Console.WriteLine("4 - Sair\n");
+            opcao = Console.ReadLine();
+            SelecaoMenu(opcao);
+        } while (opcao != "4");
+
+
+
+        void SelecaoMenu(string opcao)
+        {
+            switch (opcao)
+            {
+                case "1":
+                    clienteService.CriarConta();
+                    break;
+                case "2":
+                    Console.WriteLine("Digite o Numero da conta:");
+                    string numeroConta = Console.ReadLine();
+                    var cliente = clienteService.BuscarClientePorConta(numeroConta);
+                    if (cliente == null)
+                    {
+                        Console.WriteLine("Conta não Cadastrada");
+                    }
+                    else
+                    {
+                        Console.WriteLine(cliente.ResumoCliente());
+                    }
+                    break;
+                case "3":
+                    clienteService.ListarClientes();
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("Opção Inválida");
+                    break;
+            }
+        }
+
     }
 }
