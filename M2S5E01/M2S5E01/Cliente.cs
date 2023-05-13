ea8e9e14e7f4bdd7070c2c32840b16bac859f516
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-05-13 14:04:12 -0300

    M2S5E01

---
+++ M2S5E01/M2S5E01/Cliente.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2S5E01
+{
+    abstract class Cliente
+    {
+        private string? _nome;
+        private string? _numeroConta;
+        private double? _saldo;
+        private string? _endereco;
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public string NumeroConta
+        {
+            get
+            {
+                return _numeroConta;
+            }
+
+            set
+            { 
+                _numeroConta = value; 
+            }
+        }
+
+        public double? Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+
+            set
+            {
+                _saldo = value;
+            }
+        }
+
+        public string? Endereco
+        {
+            get
+            {
+                return _endereco;
+            }
+
+            set
+            {
+                _endereco = value;
+            }
+        }
+
+        public void ResumoCliente()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
