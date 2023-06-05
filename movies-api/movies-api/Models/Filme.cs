b5d3f9c24f4f268df26bc902aa0371667bb1bbf1
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-06-05 01:24:06 -0300

    M2S9E08

---
+++ movies-api/movies-api/Models/Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace movies_api.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Duracao { get; set; }
+    public string Diretor { get; set; }
+    public string Genero { get; set; }
+}
